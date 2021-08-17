aab93385847858873e0df46130e06b4ae0352efd
György Császár <csaszar.gyorgy890322@gmail.com>
2021-08-17 23:28:30 +0200

    Add project files.

---
+++ Models/Joke.cs
@@ -0,0 +1,15 @@
+﻿namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+        public Joke()
+        {
+        }
+
+        public int Id { get; set; }
+
+        public string Question { get; set; }
+
+        public string Answer { get; set; }
+    }
+}
